c4d87bb74f9ea174e5d45273faf85947265cb255
Alexey Grigorev <alexey.grigorev@olx.com>
2021-11-16 20:03:22 +0100

    backwards compatibility with tf older than 2.7

--- build_wheel.sh
+++ build_wheel.sh
@@ -2,13 +2,15 @@
 
 PREFIX="./tensorflow/tensorflow/lite/tools/pip_package"
 
-if [ -f "${PREFIX}/build_pip_package_with_cmake.sh" ]; then
-    # we're in tensorflow 2.7 + 
-    bash install_cmake.sh
-
-    export CI_BUILD_PYTHON=${PYTHON}
-    sh ${PREFIX}/build_pip_package_with_cmake.sh
-else
+if [ -f "${PREFIX}/build_pip_package.sh" ]; then
     # we're in tensorflow 2.6 or older - no need to install cmake
     sh ${PREFIX}/build_pip_package.sh
+else
+    if [ -f "${PREFIX}/build_pip_package_with_cmake.sh" ]; then
+        # we're in tensorflow 2.7 + 
+        bash install_cmake.sh
+
+        export CI_BUILD_PYTHON=${PYTHON}
+        sh ${PREFIX}/build_pip_package_with_cmake.sh
+    fi
 fi
