86ba6d30e77074d6765660125b4735dc2d1bc4e9
Ubuntu <ubuntu@ip-172-31-43-14.eu-west-1.compute.internal>
2023-11-23 20:52:35 +0000

    cmake for aarch64

--- install_cmake.sh
+++ install_cmake.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env bash
 
-wget https://github.com/Kitware/CMake/releases/download/v3.16.8/cmake-3.16.8-Linux-x86_64.sh
+set -e
+
+ARCH=`uname -i`
 mkdir /opt/cmake
-sh cmake-3.16.8-Linux-x86_64.sh --prefix=/opt/cmake --skip-license
-ln -s /opt/cmake/bin/cmake /usr/local/bin/cmake
+
+if [ "${ARCH}" == "x86_64" ]; then
+    wget https://github.com/Kitware/CMake/releases/download/v3.16.8/cmake-3.16.8-Linux-x86_64.sh
+    sh cmake-3.16.8-Linux-x86_64.sh --prefix=/opt/cmake --skip-license
+    ln -s /opt/cmake/bin/cmake /usr/local/bin/cmake
+    exit 0
+fi
+
+if [ "${ARCH}" == "aarch64" ]; then
+    #wget https://github.com/Kitware/CMake/releases/download/v3.27.8/cmake-3.27.8-linux-aarch64.sh
+    #sh cmake-3.27.8-linux-aarch64.sh --prefix=/opt/cmake --skip-license
+    wget https://cmake.org/files/v3.19/cmake-3.19.3-Linux-aarch64.sh
+    sh cmake-3.19.3-Linux-aarch64.sh --prefix=/opt/cmake --skip-license
+    ln -s /opt/cmake/bin/cmake /usr/local/bin/cmake
+    exit 0
+fi
+
+echo "unknown architecture ${ARCH}"
+exit 1
